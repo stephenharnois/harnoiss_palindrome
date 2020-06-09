22ea3e53ba635a2596d49b70d58594af4ecf05a4
Stephen Harnois <stephen.harnois@live.com>
2020-06-09 16:17:13 -0400

    Add integer palindrome support

--- lib/harnoiss_palindrome.rb
+++ lib/harnoiss_palindrome.rb
@@ -1,16 +1,23 @@
 require "harnoiss_palindrome/version"
 
-class String
-
+module HarnoissPalindrome
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+  processed_content == processed_content.reverse
   end
 
   private
 
-    # Returns content for palindrome testing.
-    def processed_content
-      scan(/[a-z]/i).join.downcase
-    end
+  # Returns content for palindrome testing.
+  def processed_content
+    to_s.scan(/[a-z0-9]/i).join.downcase
+  end
+end
+
+class String
+  include HarnoissPalindrome
+end
+
+class Integer
+  include HarnoissPalindrome
 end
